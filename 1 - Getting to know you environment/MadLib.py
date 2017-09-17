9e55cc951d79f2fbe726369a620db3c053f12d7a
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2017-09-16 21:10:28 -0700

    First commit

---
+++ 1 - Getting to know you environment/MadLib.py
@@ -0,0 +1,11 @@
+# MadLib.py
+adjective = input("Please enter an adjective: ")
+noun = input("Please enter a noun: ")
+verb = input("Please enter a verb ending in -ed: ")
+adjective2 = input("Please enter an adjective: ")
+color = input("Please enter a color: ")
+animal = input("Please enter an animal: ")
+print("Your MadLib:")
+print("The", adjective, noun, verb, "over the", adjective2, color, animal, ".")
+
+            
