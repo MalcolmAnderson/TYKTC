175e228e1d75a62dc8af4f30d5e61177aad3ebbc
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2017-10-24 00:15:43 -0700

    playing with color

--- 02 - SquareSpiral/03_CircleSpiral.py
+++ 02 - SquareSpiral/03_CircleSpiral.py
@@ -2,6 +2,7 @@
 
 import turtle
 t = turtle.Pen()
+t.penColor("red")
 t.speed(0)
 for x in range(200):
     t.circle(x)
---
+++ 02 - SquareSpiral/04_SquareSpiral.py
@@ -0,0 +1,9 @@
+# 04_SquareSpiral.py
+import turtle
+t = turtle.Pen()
+t.speed(0)
+t.pencolor("salmon")
+for x in range(300):
+    t.forward(1*x)
+    t.left(91)
+    
